f930847053befc5684d7a1d869540fb5348e7fb3
SebastianSotoAr <ssotoa@unal.edu.co>
2022-10-06 20:53:42 -0500

    try 5

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -10,11 +10,12 @@ public class Tesis extends Escrito {
 	public Tesis(String origen, String titulo, String autor, int paginas) {
 		super(origen, titulo, autor, paginas);
 	}
-
-	public Tesis(String origen, String titulo, String autor, int paginas, String idea, String conclusion,
-			String referencias, String interpretacion) {
+	
+	public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos,
+			String conclusion, String referencias, String interpretacion) {
 		super(origen, titulo, autor, paginas);
 		this.idea = idea;
+		this.argumentos = argumentos;
 		this.conclusion = conclusion;
 		this.referencias = referencias;
 		this.interpretacion = interpretacion;
